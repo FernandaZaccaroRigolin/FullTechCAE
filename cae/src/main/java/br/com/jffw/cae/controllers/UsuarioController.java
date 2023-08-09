5ee38f3716a5279e23b2befa411f9f95dfaadb16
Felipe Soares <felipeaugustodasilvasoares@gmail.com>
2023-08-09 09:00:12 -0300

    Realizado alteração no arquivo alterarUsuario

--- cae/src/main/java/br/com/jffw/cae/controllers/UsuarioController.java
+++ cae/src/main/java/br/com/jffw/cae/controllers/UsuarioController.java
@@ -1,11 +1,23 @@
 package br.com.jffw.cae.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.jffw.cae.models.Usuario;
+import br.com.jffw.cae.repository.UsuarioRepository;
+
 @Controller
+@RequestMapping("/usuarios")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
 	@GetMapping("/usuarios/novo")
 	public ModelAndView incluir() {
@@ -16,4 +28,25 @@ public class UsuarioController {
 		}
 	}
 
+	@GetMapping("/alterar/{doc}")
+	public ModelAndView alterar(@PathVariable("doc") Integer id) {
+		try {
+			Usuario usuario = usuarioRepository.getReferenceById(id);
+			return new ModelAndView("usuarios/alterarUsuario", "usuario", usuario);
+			
+		} catch (Exception e) {
+			return new ModelAndView("erro", "msg_erro", e.toString());
+		}
+	}
+	
+	@PostMapping("/alterar")
+	public String alterar(Usuario usuario, Model model) {
+		try {
+			usuarioRepository.save(usuario);
+			return "redirect:/usuario/lista";
+		} catch (Exception e) {
+			model.addAttribute("msg_erro", e.toString());
+			return "erro";
+		}
+	}
 }
