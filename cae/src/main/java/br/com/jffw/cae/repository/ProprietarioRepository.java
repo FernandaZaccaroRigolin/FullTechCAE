cf9d15400377488993c74b2889d4b148a5ff31ef
Felipe Augusto Da Silva Soares <BRX0002126@prosegur.com>
2023-08-14 13:51:19 -0300

    Realizando o CRUD de Proprietarios

--- cae/src/main/java/br/com/jffw/cae/repository/ProprietarioRepository.java
+++ cae/src/main/java/br/com/jffw/cae/repository/ProprietarioRepository.java
@@ -4,16 +4,24 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import br.com.jffw.cae.dto.ApartamentoProprietariosDTO;
 
-import br.com.jffw.cae.dto.ProprietarioApartamentosDTO;
 import br.com.jffw.cae.models.Proprietario;
 
 public interface ProprietarioRepository extends JpaRepository<Proprietario, String> {
 	
-	@Query("SELECT new br.com.jffw.cae.dto.ProprietarioApartamentosDTO("
-			+ " a.id, p.cpf, p.nome, a.numeroApartamento, a.blocoApartamento) "
-			+ " FROM Proprietario p INNER JOIN p.apartamentos a")
-	List<ProprietarioApartamentosDTO> getProprietarioApartamentosDTO();
+	@Query("SELECT new br.com.jffw.cae.dto.ApartamentoProprietariosDTO("
+			+ " p.cpf, a.id, a.numero, a.bloco, a.qndVagas) "
+			+ " FROM Apartamento a INNER JOIN a.proprietarios p")
+	List<ApartamentoProprietariosDTO> getApartamentoProprietariosDTO();
+	
+	@Query("SELECT new br.com.jffw.cae.dto.ApartamentoProprietariosDTO("
+			+ " p.cpf, a.id, a.numero, a.bloco, a.qndVagas) "
+			+ " FROM Apartamento a INNER JOIN a.proprietarios p WHERE p.cpf = ?1")
+	List<ApartamentoProprietariosDTO> getApartamentoProprietariosDTOByCpf(String cpf);
+
+
+	
 
 }
 
