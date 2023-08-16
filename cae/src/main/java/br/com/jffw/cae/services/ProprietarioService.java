95f1f2f82f80d1d218af8a871aab6562a34210b8
Felipe Soares <felipeaugustodasilvasoares@gmail.com>
2023-08-16 11:39:56 -0300

    Realizado alterações no CRUD Proprietario, para ajuste de rotas e tratativas de erro.

--- cae/src/main/java/br/com/jffw/cae/services/ProprietarioService.java
+++ cae/src/main/java/br/com/jffw/cae/services/ProprietarioService.java
@@ -1,15 +1,19 @@
 package br.com.jffw.cae.services;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.jffw.cae.dto.ProprietarioDTO;
+import br.com.jffw.cae.dto.ProprietarioListDTO;
+import br.com.jffw.cae.models.Apartamento;
 import br.com.jffw.cae.models.Proprietario;
 import br.com.jffw.cae.repository.ApartamentoRepository;
 import br.com.jffw.cae.repository.ProprietarioRepository;
@@ -19,61 +23,127 @@ public class ProprietarioService {
 
 	@Autowired
 	private ProprietarioRepository proprietarioRepository;
-	
+
 	@Autowired
 	private ApartamentoRepository apartamentoRepository;
 
-	public List<Proprietario> listarProprietarios() {
-		return proprietarioRepository.findAll();
-	}
-	
-	public Optional<Proprietario> listarProprietariosId(String cpf){
-		return proprietarioRepository.findById(cpf);
-	}
+	public ProprietarioListDTO buscarProprietarioPorCpf(String cpf) {
+		Proprietario proprietario = proprietarioRepository.findById(cpf)
+				.orElseThrow(() -> new IllegalArgumentException("Proprietário não encontrado"));
 
-	public List<ProprietarioDTO> listarProprietariosDTO() {
-		List<ProprietarioDTO> proprietarios = new ArrayList<ProprietarioDTO>();
+		ProprietarioListDTO dto = new ProprietarioListDTO();
+		dto.setCpf(proprietario.getCpf());
+		dto.setNome(proprietario.getNome());
+		dto.setTelefone(proprietario.getTelefone());
+		dto.setDtNascimento(proprietario.getDtNascimento());
+		dto.setEmail(proprietario.getEmail());
 
-		proprietarioRepository.findAll().forEach(c -> proprietarios
-				.add(new ProprietarioDTO(c.getCpf(), c.getNome(), c.getTelefone(), c.getDtNascimento(), c.getEmail())));
-
-		return proprietarios;
-
-	}
-
-	public ProprietarioDTO buscarProprietario(String cpf) {
-		Proprietario proprietario = proprietarioRepository.getReferenceById(cpf);
-		return new ProprietarioDTO(
-				proprietario.getCpf(),
-				proprietario.getNome(),
-				proprietario.getTelefone(),
-				proprietario.getDtNascimento(),
-				proprietario.getEmail());
-	}
-
-	public Proprietario incluirProprietario(Proprietario proprietario) {
-		
-		Optional<Proprietario> prop = proprietarioRepository.findById(proprietario.getCpf());
-		if(!prop.isEmpty()) {
-			throw new RuntimeException("Este proprietario já existe");
+		Apartamento apartamento = proprietario.getApartamento();
+		if (apartamento != null) {
+			dto.setApartamento(apartamento.getId());
+			dto.setNumeroApartamento(apartamento.getNumero());
+			dto.setBlocoApartamento(apartamento.getBloco());
+			dto.setQndVagasApartamento(apartamento.getQndVagas());
 		}
-		return proprietarioRepository.save(proprietario);
+
+		dto.setDtNascimentoFormatted(dto.getDtNascimentoFormatted());
+
+		return dto;
 	}
 
-	public Proprietario alterar(Proprietario proprietario, String cpf) {
-		Proprietario propratual = proprietarioRepository.getReferenceById(cpf);
-		if (propratual == null) {
-			throw new EntityNotFoundException("o Proprietario a ser alterado não existe.");
+	public List<ProprietarioListDTO> listarProprietariosDTO() {
+		List<Proprietario> proprietarios = proprietarioRepository.findAll();
+		List<ProprietarioListDTO> dtos = new ArrayList<>();
+
+		for (Proprietario proprietario : proprietarios) {
+			ProprietarioListDTO dto = new ProprietarioListDTO();
+			dto.setCpf(proprietario.getCpf());
+			dto.setNome(proprietario.getNome());
+			dto.setTelefone(proprietario.getTelefone());
+			dto.setDtNascimento(proprietario.getDtNascimento());
+			dto.setEmail(proprietario.getEmail());
+
+			Apartamento apartamento = proprietario.getApartamento();
+			if (apartamento != null) {
+				dto.setApartamento(apartamento.getId());
+				dto.setNumeroApartamento(apartamento.getNumero());
+				dto.setBlocoApartamento(apartamento.getBloco());
+				dto.setQndVagasApartamento(apartamento.getQndVagas());
+			}
+
+			dto.setDtNascimentoFormatted(dto.getDtNascimentoFormatted());
+
+			dtos.add(dto);
 		}
-		propratual.setNome(proprietario.getNome());
-		return proprietarioRepository.save(propratual);
+
+		return dtos;
 	}
+
+	public Proprietario incluirProprietario(ProprietarioDTO dto) throws ParseException {
+		String cpf = dto.getCpf();
+		Proprietario existingProprietario = proprietarioRepository.findById(cpf).orElse(null);
+		if (existingProprietario != null) {
+			throw new IllegalArgumentException("O proprietário já está cadastrado.");
+		}
+
+		Proprietario proprietario = new Proprietario();
+		proprietario.setCpf(cpf);
+		proprietario.setNome(dto.getNome());
+		proprietario.setTelefone(dto.getTelefone());
+		proprietario.setDtNascimento(dto.getDtNascimento());
+		proprietario.setEmail(dto.getEmail());
+
+		Integer idApartamento = dto.getApartamento();
+		if (idApartamento != null) {
+			Apartamento apartamento = apartamentoRepository.findById(idApartamento).orElse(null);
+			if (apartamento == null) {
+				throw new IllegalArgumentException("O ID do apartamento não existe.");
+			}
+
+			List<Proprietario> proprietariosDoApartamento = apartamento.getProprietarios();
+			if (!proprietariosDoApartamento.isEmpty()) {
+				throw new IllegalArgumentException("O apartamento já está vinculado a outro proprietário.");
+			}
+
+			proprietario.setApartamento(apartamento);
+		}
+
+		proprietarioRepository.save(proprietario);
+		return proprietario;
+	}
+
+	public ResponseEntity<?> alterar(ProprietarioDTO dto, String cpf) {
+	    Proprietario propratual = proprietarioRepository.findById(cpf)
+	            .orElseThrow(() -> new EntityNotFoundException("O Proprietario a ser alterado não existe."));
+
+	    propratual.setNome(dto.getNome());
+	    propratual.setTelefone(dto.getTelefone());
+	    propratual.setDtNascimento(dto.getDtNascimento());
+	    propratual.setEmail(dto.getEmail());
+
+	    Integer idApartamento = dto.getApartamento();
+	    if (idApartamento != null) {
+	        Apartamento apartamento = apartamentoRepository.findById(idApartamento)
+	                .orElseThrow(() -> new IllegalArgumentException("O ID do apartamento não existe."));
+	        List<Proprietario> proprietariosDoApartamento = apartamento.getProprietarios();
+	        if (!proprietariosDoApartamento.isEmpty()) {
+	            throw new IllegalArgumentException("O apartamento já está vinculado a outro proprietário.");
+	        }
+	        propratual.setApartamento(apartamento);
+	    }
+
+	    proprietarioRepository.save(propratual);
+
+	    return ResponseEntity.status(HttpStatus.ACCEPTED).body("Alteração realizada com sucesso");
+	}
+
+
 
 	public String remover(String cpf) {
 		try {
-		proprietarioRepository.deleteById(cpf);
-		return String.format("Proprietario %s removido com sucesso!", cpf);
-		}catch (Exception e) {
+			proprietarioRepository.deleteById(cpf);
+			return String.format("Proprietario %s removido com sucesso!", cpf);
+		} catch (Exception e) {
 			return e.toString();
 		}
 	}
