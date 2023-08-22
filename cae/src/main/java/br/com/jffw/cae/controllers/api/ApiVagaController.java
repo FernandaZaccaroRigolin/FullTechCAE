b40cacc50e7c7270c675b70ec2a72ac5651ce389
Wanderson Mendes <wanderson.mendes@capgemini.com>
2023-08-22 15:33:55 -0300

    ajustes das validações na vaga

--- cae/src/main/java/br/com/jffw/cae/controllers/api/ApiVagaController.java
+++ cae/src/main/java/br/com/jffw/cae/controllers/api/ApiVagaController.java
@@ -16,9 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.jffw.cae.dto.ApartamentoDTO;
 import br.com.jffw.cae.dto.VagaDTO;
-import br.com.jffw.cae.models.Vaga;
 import br.com.jffw.cae.services.VagaService;
 
 @RestController @RequestMapping("/api/vagas")
@@ -32,6 +30,16 @@ public class ApiVagaController {
 		return vagaService.listarVagasDTO();
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> buscarVagaPorId(@PathVariable String id) {
+		try {
+			VagaDTO dto = vagaService.buscarVaga(id);
+			return ResponseEntity.ok(dto);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+	
 	@PostMapping("/")
 	public ResponseEntity<?> incluirVaga(@RequestBody Map<String, String> dados) {
 	    try {
@@ -40,7 +48,7 @@ public class ApiVagaController {
 	    } catch (IllegalArgumentException e) {
 	        return ResponseEntity.badRequest().body(e.getMessage()); // Resposta 400 Bad Request
 	    } catch (Exception e) {
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.toString()); // Resposta 500 Internal Server Error
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage()); // Resposta 500 Internal Server Error
 	    }
 	}
 	
@@ -50,7 +58,7 @@ public class ApiVagaController {
 			return new ResponseEntity<String>(vagaService.alterarVaga(dados, id), HttpStatus.CREATED);
 			
 		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.toString());
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
 		}
 	}	
 	
@@ -59,7 +67,7 @@ public class ApiVagaController {
 		try {
 			return new ResponseEntity<String>(vagaService.remover(id), HttpStatus.ACCEPTED);
 		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.toString());
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
 		}
 	}
 }
